d7bfcf85965275aaf0b9c32bbca6dd5692397f23
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-12-30 15:17:17 +0700

    f: menambah halaman search

--- kelompok-f/app/route.py
+++ kelompok-f/app/route.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
 
 router = Blueprint('route', __name__, template_folder='template')
 
 @router.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
+@router.route('/search')
+def search():
+  keyword = request.args.get('q')
+  return render_template('search.html', keyword=keyword)
